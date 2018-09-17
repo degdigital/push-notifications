4205a3aa5b7e6ee30d8551261f5bc25210b8bd34
Ryan Heap <rheap@degdigital.com>
2018-09-17 17:10:04 -0500

    added notification actions

--- client/sw.js
+++ client/sw.js
@@ -1,15 +1,38 @@
 const applicationServerKey = 'BF-nw-iRfSC3avyL8Xuc3vA_VBEkLcDUuGsxo6yOOBwdNMnf0KusoUiYnsUTp3JlfKHWzFSBwR3ZvXcj0ypBcp0';
 
 self.addEventListener('push', e => {
+    const messageData = e.data.json();
+
     console.log('[Service Worker] Push Received.');
-    console.log(`[Service Worker] Push had this data: "${e.data.text()}"`);
+    console.log(`[Service Worker] Push had this data: "${messageData}"`);
   
     const title = 'Push Notifications Playground';
     const options = {
-      body: e.data.text(),
-      icon: 'images/icon.png',
-      badge: 'images/badge.png'
+        body: messageData.message,
+        icon: 'images/icon.png',
+        badge: 'images/badge.png',
+        data: messageData,
+        actions: [
+            {action: 'view', title: 'View video'},
+            {action: 'close', title: 'Close'},
+        ]
     };
   
     e.waitUntil(self.registration.showNotification(title, options));
-  });
+});
+
+self.addEventListener('notificationclick', function(e) {
+    if (e.action === 'close') {
+        notification.close();
+    } else {
+        const {type, id} = e.notification.data;
+        switch(type) {
+            case 'video':
+                const url = `http://localhost:3000/video.html?id=${id}`;
+                clients.openWindow(url);
+                break;
+        } 
+    }
+
+    notification.close();
+});
--- server/pushMessage.js
+++ server/pushMessage.js
@@ -1,13 +1,19 @@
 const webpush = require('web-push');
 const clientSubscriptions = require('./clientSubscriptions.json');
  
-const payload = 'You just got pushed!';
+const payload = {
+    message: 'A new video has been posted!',
+    type: 'video',
+    id: '1'
+};
+
+var payloadBuffer = Buffer.from(JSON.stringify(payload));
    
 const options = {
     vapidDetails: {
-      subject: 'mailto:rheap@degdigital.com',
-      publicKey: 'BF-nw-iRfSC3avyL8Xuc3vA_VBEkLcDUuGsxo6yOOBwdNMnf0KusoUiYnsUTp3JlfKHWzFSBwR3ZvXcj0ypBcp0',
-      privateKey: 'bVYlg8mReDo3ZvOAGvVMmHQ-yhBEQbuHTeE3zyOzyuY'
+        subject: 'mailto:rheap@degdigital.com',
+        publicKey: 'BF-nw-iRfSC3avyL8Xuc3vA_VBEkLcDUuGsxo6yOOBwdNMnf0KusoUiYnsUTp3JlfKHWzFSBwR3ZvXcj0ypBcp0',
+        privateKey: 'bVYlg8mReDo3ZvOAGvVMmHQ-yhBEQbuHTeE3zyOzyuY'
     },
     TTL: 1800,
     headers: {
@@ -17,17 +23,17 @@ const options = {
 }
 
 const promises = clientSubscriptions.map(clientSubscription => {
-    return webpush.sendNotification(clientSubscription.subscription, payload, options)
-      .then(() => true)
-      .catch(error => {
-        console.log('ERROR: ', error);
-        return false;
-      });
+    return webpush.sendNotification(clientSubscription.subscription, payloadBuffer, options)
+        .then(() => true)
+        .catch(error => {
+            console.log('ERROR: ', error);
+            return false;
+        });
 });
 
 Promise.all(promises)
-  .then(results => {
-    const successes = results.filter(result => result); 
-    console.log(`${successes.length} message(s) pushed`)
-  });
+    .then(results => {
+        const successes = results.filter(result => result); 
+        console.log(`${successes.length} message(s) pushed`)
+    });
  
